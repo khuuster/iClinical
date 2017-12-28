9519443a7458d324d1f56f916d87b21287602759
ryan <ryfield12@gmail.com>
2017-12-28 14:12:49 -0800

    pull

--- Frontend/Controllers/navBarController.js
+++ Frontend/Controllers/navBarController.js
@@ -1,13 +1,18 @@
 app.controller("navBarController", function ($scope, $state, $stateParams, userService, companyService) {
-  if (companyService.currentCompanyReturn == 0) {
+  if (companyService.currentCompanyReturn() == 0) {
     $scope.companyDash = true; 
   } else {
     $scope.companyDash = false; 
   }
+  if (userService.currentUserReturn() == 0) {
+    $scope.userDash = true; 
+  } else {
+    $scope.userDash = false; 
+  }
   console.log($scope.companyDash);
   // logout function
   $scope.logout = function(){
-    if (companyService.currentCompanyReturn == 0) {
+    if (companyService.currentCompanyReturn() == 0) {
       $scope.companyDash = true; 
     } else {
       $scope.companyDash = false;
